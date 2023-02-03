f72316b8af8a8925420e81403892bfa05b3c15bc
Praise Ebuka <praise.njoga@gmail.com>
2023-02-03 20:07:20 +0100

    Am really stressed-out right now, but i'll keep you posted sha.............I've got an upcoming Exam thats why

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('is_admin')->default('false');
+            $table->boolean('is_admin');
             $table->rememberToken();
             $table->timestamps();
         });
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        //The first four lines of codes where part of the package
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
+        //where my own code started from.
+
+        $this->call(Admin::class);
     }
 }
