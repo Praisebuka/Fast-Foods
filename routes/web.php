df0b20397f64540f052f4179c307f1ad8aebed12
Praise Ebuka <praise.njoga@gmail.com>
2023-05-30 20:54:46 +0100

    Still trying to link both the Admin and Users to their seprate files after the AUTH process.

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
